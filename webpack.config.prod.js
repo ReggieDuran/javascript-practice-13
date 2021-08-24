082bc1d5a1e7f618ab525c84e46564c80879cb98
Reggie <reggieduran15@gmail.com>
2021-08-24 13:15:23 +0800

    feature detection + polyfill

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -22,7 +22,13 @@ module.exports = {
           loader: 'babel-loader',
           options: {
             presets: [
-              ['@babel/preset-env', { targets: 'defaults' }]
+              ['@babel/preset-env', 
+                { 
+                  targets: 'defaults', 
+                  useBuiltIns: 'usage', 
+                  corejs: {version: 3} 
+                }
+              ]
             ]
           }
         }
